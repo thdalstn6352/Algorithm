5d76b9aa4a9662e0b1ecf2d0a4dea0358f99fbac
minsoo <thdalstn6352@naver.com>
2021-11-28 22:08:46 +0900

    Modify Algorithm

--- Algorithm/src/baekjoon/BOJ_2565_전깃줄/Main.java
+++ Algorithm/src/baekjoon/BOJ_2565_전깃줄/Main.java
@@ -16,14 +16,11 @@ public class Main {
             String[] line = br.readLine().split(" ");
             wireList.add(new Wire(Integer.parseInt(line[0]), Integer.parseInt(line[1])));
         }
-        //시작하는 전깃줄을 기준으로 정렬
         Collections.sort(wireList);
 
         //LIS 알고리즘
         for(int i = 0; i < N; i++) {
-            //모든 dp배열 1로 초기화
             dp[i] = 1;
-            //현재 전깃줄 이전의 모든 전깃줄과 비교
             for(int j = 0; j < i; j++) {
                 if(wireList.get(j).start < wireList.get(i).start && wireList.get(j).end < wireList.get(i).end) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
