91f9fea584e5f8f405df20d01a3511c45c4771b4
thdalstn6352 <thdalstn6352@naver.com>
2022-01-11 21:20:00 +0900

    BOJ 소용돌이 code

---
+++ Algorithm/src/baekjoon/BOJ_1022_소용돌이/소용돌이.py
@@ -0,0 +1,21 @@
+r1, c1, r2, c2 = map(int, input().split())
+
+data = [[0 for _ in range(c2 - c1 + 1)] for _ in range(r2 - r1 + 1)]
+
+print(data)
+print(r1, r2, c1, c2)
+
+
+
+
+
+
+# 좌상 구하기
+index1 = (abs(r1) + 1) * 2 - 1
+index2 =
+leftup = (index ** 2) - ((index - 1) * 2)
+rightdown =
+print(leftup)
+# 우하 -> 1 3 5 7 9  ... 제곱
+# 좌상 -> 1 3제곱 - ((3 - 1) * 2) 25 - 8 = 17 / 49 - 12
+
