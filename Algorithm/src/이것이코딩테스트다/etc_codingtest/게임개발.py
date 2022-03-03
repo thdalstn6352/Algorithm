50bb79ebac8b727ed13288c41f6b64a6d98568e4
thdalstn6352 <thdalstn6352@naver.com>
2022-03-03 23:30:31 +0900

    이것이 코딩테스트다 게임게발 코드

--- Algorithm/src/이것이코딩테스트다/etc_codingtest/게임개발.py
+++ Algorithm/src/이것이코딩테스트다/etc_codingtest/게임개발.py
@@ -1,45 +1,50 @@
 n, m = map(int, input().split())
-a, b, dir = map(int, input().split())
-check = 0
-move = 1
-
+x, y, direction = map(int, input().split())
 isVisited = [[0] * m for _ in range(n)]
-# print(isVisited)
-isVisited[a][b] = 1
 
-#     북  동  남 서
+isVisited[x][y] = 1
+
+array = []
+for i in range(n):
+    array.append(list(map(int, input().split())))
+
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-board = list()
-for _ in range(n):
-    board.append(list(map(int, input().split())))
+# 왼쪽으로 회전
+def turn_left():
+    global direction
+    direction -= 1
+    if direction == -1:
+        direction = 3
 
+# 시작
+count = 1
+turn_time = 0
 while True:
-    temp = dir
-    dir = dir - 1
-    if dir == -1:
-        dir = 3
-
-    na = a + dx[dir]
-    nb = b + dy[dir]
-
-    if isVisited[na][nb] != 1 and board[na][nb] != 1:
-        isVisited[na][nb] = 1
-        a, b = na, nb
-        check = 0
-        move += 1
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+    if isVisited[nx][ny] == 0 and array[nx][ny] == 0:
+        isVisited[nx][ny] = 1
+        x = nx
+        y = ny
+        count += 1
+        turn_time = 0
         continue
     else:
-        check += 1
+        turn_time += 1
 
-    if check == 4:
-        na = a - dx[temp]
-        nb = b - dy[temp]
-        if board[na][nb] == 1:
+    if turn_time == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+
+        # 뒤로 갈 수 있으면
+        if array[nx][ny] == 0:
+            x = nx
+            y = ny
+        else:
             break
-        a, b = na, nb
-        check = 0
+        turn_time = 0
 
-
-print(move)
+print(count)
